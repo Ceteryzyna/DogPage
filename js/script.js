e5b68e36a3829fdeb7a06846d15c6e5c1e4f83d1
Paula Andrzejewska <haftpaula@gmail.com>
2023-10-27 12:35:23 +0200

    add event to hide menu after click

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const nav = document.querySelector('.navbar')
+document.addEventListener("DOMContentLoaded", function () {
+	const nav = document.querySelector(".navbar");
+	const navLinksItems = document.querySelectorAll(".nav-link");
+	const navList = document.querySelector(".navbar-collapse");
 
 	function addShadow() {
 		if (window.scrollY >= 100) {
-			nav.classList.add('shadow-bg')
+			nav.classList.add("shadow-bg");
 		} else {
-			nav.classList.remove('shadow-bg')
+			nav.classList.remove("shadow-bg");
 		}
 	}
 
-    window.addEventListener('scroll', addShadow)
-})
+	navLinksItems.forEach(item =>
+		item.addEventListener("click", () => {
+			navList.classList.remove("show");
+		})
+	);
+	window.addEventListener("scroll", addShadow);
+});
